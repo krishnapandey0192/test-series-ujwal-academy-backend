6192677f8a6f5cf09a85a12d91f89e5cb347d595
krishnapandey0192 <krishnapandey0206@gmail.com>
2025-08-18 11:56:23 +0530

    fix/cors

--- server.js
+++ server.js
@@ -38,10 +38,7 @@ initializeServer();
 // Middleware
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://test-series-ujwal-academy.vercel.app/",
-    ], // your frontend URL
+    origin: "https://test-series-ujwal-academy.vercel.app", // your frontend URL
     credentials: true,
   })
 );
