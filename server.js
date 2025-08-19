b3830073d5ea4f363a8bec5e5c710284752ade73
Krishna Pandey <121028355+krishnapandey0192@users.noreply.github.com>
2025-08-19 19:54:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ initializeServer();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173", // your frontend URL
+    origin: "https://test-series-ujwal-academy.vercel.app/", // your frontend URL
     credentials: true,
   })
 );
