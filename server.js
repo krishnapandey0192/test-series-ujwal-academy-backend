8a71d2d6ff32126bab31d5ac9d50e255821508e6
Krishna Pandey <121028355+krishnapandey0192@users.noreply.github.com>
2025-10-28 10:40:49 +0530

    Update server.js comment credentials

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(
   cors({
     "https://test-series-ujwal-academy.vercel.app"
     // origin: "http://localhost:5173", // your frontend URL
-    credentials: true,
+    // credentials: true,
   })
 );
 app.use(express.json({ limit: "10mb" }));
